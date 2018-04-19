54cbae177a07b0b18ecfb435d30a2b3fd7094531
Allan <allancarlosdeveloper@gmail.com>
2018-04-19 01:40:34 -0300

    refactor: stateful to stateless

--- src/pages/BooksList.js
+++ src/pages/BooksList.js
@@ -1,61 +1,62 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { Dimmer } from "semantic-ui-react";
 import Shelf from "../components/Shelf";
 import Quote from "../components/Quote";
 
-export default class BookList extends Component {
-  render() {
-    let { books, updateShelf, loading } = this.props;
-    const { quote } = this.props;
+export default function BookList({ books, updateShelf, loading, quote }) {
+  const shelves = [
+    {
+      title: "🤩 Currently reading",
+      shelf: "currentlyReading",
+      books,
+      updateShelf
+    },
+    { title: "🤗 Want to read", shelf: "wantToRead", books, updateShelf },
+    { title: "😎 Read", shelf: "read", books, updateShelf }
+  ];
 
-    const shelves = [
-      {
-        title: "🤩 Currently reading",
-        shelf: "currentlyReading",
-        books,
-        updateShelf
-      },
-      { title: "🤗 Want to read", shelf: "wantToRead", books, updateShelf },
-      { title: "😎 Read", shelf: "read", books, updateShelf }
-    ];
-
-    return (
-      <Dimmer.Dimmable blurring dimmed={loading}>
-        <Dimmer inverted active={loading}>
-          <Quote quote={quote} />
-        </Dimmer>
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1><img src="images/kitty.svg" alt="" /> MyReads</h1>
-          </div>
-
-          <div className="list-books-content">
-            {shelves.map(shelf => (
-              <Shelf
-                key={shelf.shelf}
-                title={shelf.title}
-                shelf={shelf.shelf}
-                books={shelf.books}
-                updateShelf={shelf.updateShelf}
-              />
-            ))}
-          </div>
+  return (
+    <Dimmer.Dimmable blurring dimmed={loading}>
+      <Dimmer inverted active={loading}>
+        <Quote quote={quote} />
+      </Dimmer>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>
+            <img src="images/kitty.svg" alt="" /> MyReads
+          </h1>
         </div>
 
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
+        <div className="list-books-content">
+          {shelves.map(shelf => (
+            <Shelf
+              key={shelf.shelf}
+              title={shelf.title}
+              shelf={shelf.shelf}
+              books={shelf.books}
+              updateShelf={shelf.updateShelf}
+            />
+          ))}
         </div>
-      </Dimmer.Dimmable>
-    );
-  }
+      </div>
+
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </Dimmer.Dimmable>
+  );
 }
 
 BookList.propTypes = {
   loading: PropTypes.bool,
   books: PropTypes.array.isRequired,
-  updateShelf: PropTypes.func.isRequired
+  updateShelf: PropTypes.func.isRequired,
+  quote: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.object,
+  ])
 };
 BookList.defaultProps = {
   loading: null
